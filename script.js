f13632d26af6ef74fe0da2fd034b30c77e3b3fad
xandmanx <xfacter02@gmail.com>
2019-05-11 13:21:50 -0700

    finessed fence calculator

--- script.js
+++ script.js
@@ -16,20 +16,27 @@ const fenceOutputMax = document.querySelector('#fenceOutputMax');
 
 fenceButton.addEventListener('click', () => {
     let b = fenceInputLength.value;
-    let a = fenceInputAreas.value - -1;
+    let a = fenceInputAreas.value- -1;
     let x = b / (2 * a);
-    let y = b - (3 * x);
+    let y = b - (a * x);
     let max = x * y;
-    fenceOutputX.textContent = x;
-    fenceOutputY.textContent = y;
-    fenceOutputMax.textContent = max; 
+    let r = a % 1;
+
+    //only runs output if values are within real parameters
+    
+    if (b > -0.999999999 && a > 1 && r == 0) {
+        fenceOutputX.textContent = x;
+        fenceOutputY.textContent = y;
+        fenceOutputMax.textContent = max; 
+    } else {
+        alert("Please enter a valid number.");
+    };
 });
+
 
 //prevents page from refreshing upon calculation
 
-fenceForm.addEventListener('submit', function(fenceButton){fenceButton.preventDefault();});
-
-
+fenceForm.addEventListener('submit', function(fenceForm){fenceForm.preventDefault();});
 
 
 
